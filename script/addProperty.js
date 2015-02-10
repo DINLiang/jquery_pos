b26f3be24dd79c7fb38183c7a47667bc4fa032c2
caolijian <619060342@qq.com>
2015-02-10 16:34:32 +0800

    优化

---
+++ script/addProperty.js
@@ -0,0 +1,24 @@
+/**
+ * Created by caolijian on 15-2-9.
+ */
+function addPrototypeMain()
+{
+    $(document).ready(function()
+    {
+        $('#savePrototype').click(function()
+        {
+            addPrototype();
+        });
+    })
+}
+function addPrototype()
+{
+    var propertyStr=localStorage.getItem('propertyDb');
+    var property=JSON.parse(propertyStr);
+    var inputs=document.getElementsByName('newPrototypeName');
+    var name=inputs[0].value+":";
+    property.push(name);
+    propertyStr=JSON.stringify(property);
+    property=localStorage.setItem('propertyDb',propertyStr);
+    $.alert('添加成功')
+}
